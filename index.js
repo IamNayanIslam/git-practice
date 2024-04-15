8d76498a741b9c05487780847ebf60f7a131d375
IamNayanIslam <iamnayanislam@gmail.com>
2024-04-15 19:46:38 +0600

    git commands cleared

--- index.js
+++ index.js
@@ -3,11 +3,3 @@ console.log("Hello World");
 function add(a, b) {
   return a + b;
 }
-
-
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/IamNayanIslam/git-practice.git
-git push -u origin main
